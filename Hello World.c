a76a14e435c4384ece588498b9a4070cceb8936b
Sheel Parekh <sdparek2@illinois.edu>
2016-01-18 13:36:25 -0600

    Hello World writes to file

--- Hello World.c
+++ Hello World.c
@@ -1,7 +1,16 @@
 /*Write your C code here*/
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 int main() {
-    write(1, "Hi! My name is ", 15);
+    
+    mode_t mode = S_IRUSR;
+    int file = open("output.txt", O_CREAT | O_TRUNC | O_RDWR, mode);
+    write(file, "Hi! My name is ", 15);
+    close(file);
+    
+    
     return 0;
 }
